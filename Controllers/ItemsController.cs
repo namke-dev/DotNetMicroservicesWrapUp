b70675f31ba254be1fee18a3abc39f52e146a96c
namke-dev <namke.dev@gmail.com>
2023-12-19 23:25:44 +0700

    Rename paramenter of Dtos

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
 
@@ -25,10 +24,56 @@ namespace Play.Catalog.Service.Controllers
         }
 
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
-            var item = items.Where(i => i.Id == id).SingleOrDefault();
+            var item = items.Find(i => i.Id == id);
+            if (item is null)
+            {
+                return NotFound();
+            }
             return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<ItemDto> Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            int targetItemIndex = items.FindIndex(s => s.Id == id);
+            if (targetItemIndex == -1)
+            {
+                return NotFound();
+            }
+
+            var newItem = new ItemDto
+            (
+                id,
+                updateItemDto.Name,
+                updateItemDto.Description,
+                updateItemDto.Price,
+                items[targetItemIndex].CreateDate
+            );
+            items[targetItemIndex] = newItem;
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult<ItemDto> Delete(Guid id)
+        {
+            int targetItemIndex = items.FindIndex(s => s.Id == id);
+            if (targetItemIndex == -1)
+            {
+                return NotFound();
+            }
+
+            items.RemoveAt(targetItemIndex);
+            return NoContent();
         }
     }
 }
--- Dtos/Dtos.cs
+++ Dtos/Dtos.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Play.Catalog.Service.Dtos
 {
-    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreateAdte);
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreateDate);
     public record CreateItemDto(string Name, string Description, decimal Price);
     public record UpdateItemDto(string Name, string Description, decimal Price);
 }
